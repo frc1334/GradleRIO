a5887fffbbea83a965e0d783c0cc718545022e1a
Megapead <gabrielciolac@gmail.com>
2016-05-09 20:54:31 -0400

    Added Xbox360Controller to template, added subsystems util to template

---
+++ src/main/java/org/usfirst/frc/team1334/robot/Util/Subsystems.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team1334.robot.Util;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Created by gabri on 2016-05-09.
+ */
+public class Subsystems {
+
+    /**
+     * Usage:
+     *
+     * public static <ExampleSubsystem> EXAMPLE_SUBSYSTEM;
+     */
+    public Subsystems()
+    {
+        /**
+         * Usage:
+         *
+         * EXAMPLE_SUBSYSTEM = new ExampleSubsystem();
+         */
+    }
+
+
+    public static void print(Object o)
+    {
+        System.out.println(o);
+    }
+}

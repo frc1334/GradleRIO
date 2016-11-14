ed8c7eb1e8bfc9a763d20661517fff7284022209
Robert Nichita <robert.nickykitta@gmail.com>
2016-11-14 17:26:16 -0500

    Revert "Revert "4CimTank initial commit""
    This reverts commit fcd174a2b11df527ade11d2597a4414b401ed6c7.

--- src/main/java/org/usfirst/frc/team1334/robot/OI.java
+++ src/main/java/org/usfirst/frc/team1334/robot/OI.java
@@ -1,11 +1,26 @@
 package org.usfirst.frc.team1334.robot;
 
 
+import org.usfirst.frc.team1334.robot.Util.Xbox360Controller;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+    private static final Xbox360Controller Controller = new Xbox360Controller(0);
+
+
+
+    public static double getDriverSpeed()
+        {
+            return Controller.getRightTrigger()- Controller.getLeftTrigger();
+        }
+    public static double getSteer ()
+    {
+        return Controller.getLeftXAxis();
+    }
+
     /**Usage:
      *
      * private static final Xbox360Controller JS_DRIVER = new Xbox360Controller(int port,double deadzone)

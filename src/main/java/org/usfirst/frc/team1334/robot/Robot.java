fcd174a2b11df527ade11d2597a4414b401ed6c7
Megapead <gabrielciolac@gmail.com>
2016-11-14 09:54:04 -0500

    Revert "4CimTank initial commit"
    This reverts commit 1cd240f9c42efb3de86b05f88140820bdc3e6d65.

--- src/main/java/org/usfirst/frc/team1334/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team1334/robot/Robot.java
@@ -3,12 +3,9 @@ package org.usfirst.frc.team1334.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team1334.robot.Commands.DriveCommands;
-import org.usfirst.frc.team1334.robot.Util.Subsystems;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,10 +16,8 @@ import org.usfirst.frc.team1334.robot.Util.Subsystems;
  */
 public class Robot extends IterativeRobot {
 
-
     public static OI oi;
-    Command DriveCommands;
-    Subsystems sub;
+
     Command autonomousCommand;
     SendableChooser chooser;
 
@@ -31,10 +26,7 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-        sub = new Subsystems();
         oi = new OI();
-        DriveCommands = new DriveCommands();
-
         chooser = new SendableChooser();
         //chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
@@ -70,8 +62,6 @@ public class Robot extends IterativeRobot {
     }
 
     public void teleopInit() {
-        DriveCommands.start();
-
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
